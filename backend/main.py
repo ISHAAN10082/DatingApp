cb44e3026a279e7ee8795ee669b8ba3dedaa9b42
ISHAAN1082 <ishaan.majumdar01@gmail.com>
2024-08-07 19:16:28 +0530

    handling  exception cases

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
+# dependency function in FastAPI that provides a database session for each request
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+        yield db  #returns the session to the caller
     finally:
         db.close()
 
@@ -25,17 +25,24 @@ async def fetch_users(num_users: int, db: Session = Depends(get_db)):
 
 @app.get("/random_user/", response_model=schemas.User)
 async def get_random_user(db: Session = Depends(get_db)):
-    return crud.get_random_user(db)
+    user = crud.get_random_user(db)
+    if not user:
+        raise HTTPException(status_code=404, detail="No users found")
+    return user
 
 
 @app.get("/nearest_users/", response_model=list[schemas.User])
 async def get_nearest_users(uid: str, x: int, db: Session = Depends(get_db)):
-    return crud.get_nearest_users(db, uid, x)
+    users = crud.get_nearest_users(db, uid, x)
+    if not users:
+        raise HTTPException(status_code=404, detail="User not found or no nearby users")
+    return users
 
 
 @app.get("/random_username/", response_model=str)
 async def get_random_username(db: Session = Depends(get_db)):
     username = crud.get_random_username(db)
-    if username:
-        return username
-    raise HTTPException(status_code=404, detail="No users found in the database")
+    if not username:
+       raise HTTPException(status_code=404, detail="No users found in the database")
+    return username
+    
